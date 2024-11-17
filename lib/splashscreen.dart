da4a22a81a2579e5d5f513186603c9949364ca3d
NyimaG <130530807+NyimaG@users.noreply.github.com>
2024-11-17 07:39:30 -0500

    created splash screen

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:hw4/homepage.dart';
 import 'package:hw4/main.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -19,7 +20,7 @@ class _SplashScreenState extends State<SplashScreen>
     Future.delayed(const Duration(seconds: 3), () {
       //takes you to home screen after 3 seconds
       Navigator.of(context)
-          .pushReplacement(MaterialPageRoute(builder: (_) => const MyApp()));
+          .pushReplacement(MaterialPageRoute(builder: (_) => const HomePage()));
     });
   }
 
